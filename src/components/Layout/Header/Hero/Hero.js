0c9b8d2ef6230b936c33a5f8f743606c1754cd60
a-fuenzalida <a.fuenzalida1494@gmail.com>
2019-05-08 23:01:42 -0400

    arreglos al header

--- src/components/Layout/Header/Hero/Hero.js
+++ src/components/Layout/Header/Hero/Hero.js
@@ -1,15 +1,11 @@
 import React from 'react';
 
-import LupulosHeader from '../../../../assets/lupulos-header.jpg';
 import styles from './Hero.module.css';
 
 const Hero = () => (
   <>
-  <div className="jumbotron w-100 text-dark position-absolute">
-    <h1 className="display-4">Lupulos "Los Castaños"</h1>
-    <p className="lead">Aquí debería ir algo gay</p>
+  <div className={styles.Hero}>
   </div>
-  <img className={styles.Imagen} src={LupulosHeader} alt="Imagen header" />
   </>
 );
 
