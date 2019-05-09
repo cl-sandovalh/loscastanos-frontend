857201e130c1a65f01b265fb16724c1cc1d9a9b4
a-fuenzalida <a.fuenzalida1494@gmail.com>
2019-05-08 23:11:27 -0400

    arreglos al header #2

--- src/components/Layout/Header/Hero/Hero.js
+++ src/components/Layout/Header/Hero/Hero.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import Image from '../../../../assets/lupulos-header.jpg';
 import styles from './Hero.module.css';
 
 const Hero = () => (
   <>
   <div className={styles.Hero}>
+    <img src={Image} className={styles.Image} alt="Imagen header" />
   </div>
   </>
 );
