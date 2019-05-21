4403fbe57fc1a7fe850bb404d13ecc9e38b242f8
Claudio Sandoval <claignsan@gmail.com>
2019-05-21 04:28:10 -0400

    Contacto

---
+++ src/components/Contacto/ContactoForm.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const ContactoForm = (props) => {
+    return(
+<div class="d-flex justify-content-center">
+    <div class="col-4">
+        <form>
+            <div class="form-group">
+                <input class="form-control mt-3" type="text" placeholder="Nombre"></input>
+            </div>
+            <div class="form-group">
+                <input class="form-control" type="text" placeholder="Apellido"></input>
+            </div>
+            <div class="form-group">
+                <input class="form-control" type="email" placeholder="Telefono"></input>
+            </div>
+            <div class="form-group">
+                <input class="form-control" type="text" placeholder="Email"></input>
+            </div>
+            <div class="form-group">
+                <input class="form-control" type="text" placeholder="Asunto"></input>
+            </div>
+            <div class="form-group">
+                <textarea class="form-control" id="" rows="4" placeholder="Deja tu mensaje aqui"></textarea>
+            </div>
+            <div class="d-flex justify-content-center m-5">
+                <button type="submit" class="btn btn-success">Enviar</button>
+            </div>
+        </form>
+    </div>
+</div>
+    )
+}
+
+export default ContactoForm;
--- src/containers/Contacto/Contacto.js
+++ src/containers/Contacto/Contacto.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
+import ContactoForm from '../../components/Contacto/ContactoForm';
+
 class Contacto extends Component {
   render() {
     return (
-      <div>
-        Contacto
-      </div>
+      <section class="container mt-3">
+        <h3 class="text-center text-uppercase">Contactanos</h3>
+        <p class="lead text-center mt-3 ">Si tienes alguna pregunta puedes escribirnos y con gusto te responderemos en cuanto podamos</p>
+        <ContactoForm />
+      </section> 
     )
   }
 }
