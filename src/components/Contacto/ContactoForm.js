52b775da027315a89970d9392cfd38e3add6ef21
a-fuenzalida <a.fuenzalida1494@gmail.com>
2019-05-21 12:29:42 -0400

    fix columnas de contacto

--- src/components/Contacto/ContactoForm.js
+++ src/components/Contacto/ContactoForm.js
@@ -1,35 +1,35 @@
 import React from 'react';
 
 const ContactoForm = (props) => {
-    return(
-<div class="d-flex justify-content-center">
-    <div class="col-4">
-        <form>
-            <div class="form-group">
-                <input class="form-control mt-3" type="text" placeholder="Nombre"></input>
-            </div>
-            <div class="form-group">
-                <input class="form-control" type="text" placeholder="Apellido"></input>
-            </div>
-            <div class="form-group">
-                <input class="form-control" type="email" placeholder="Telefono"></input>
-            </div>
-            <div class="form-group">
-                <input class="form-control" type="text" placeholder="Email"></input>
-            </div>
-            <div class="form-group">
-                <input class="form-control" type="text" placeholder="Asunto"></input>
-            </div>
-            <div class="form-group">
-                <textarea class="form-control" id="" rows="4" placeholder="Deja tu mensaje aqui"></textarea>
-            </div>
-            <div class="d-flex justify-content-center m-5">
-                <button type="submit" class="btn btn-success">Enviar</button>
-            </div>
-        </form>
-    </div>
-</div>
-    )
+  return(
+		<div className="row d-flex justify-content-center">
+			<div className="col-md-4 col-lg-6">
+				<form>
+					<div className="form-group">
+						<input className="form-control mt-3" type="text" placeholder="Nombre"></input>
+					</div>
+					<div className="form-group">
+						<input className="form-control" type="text" placeholder="Apellido" />
+					</div>
+					<div className="form-group">
+						<input className="form-control" type="email" placeholder="Telefono" />
+					</div>
+					<div className="form-group">
+						<input className="form-control" type="text" placeholder="Email" />
+					</div>
+					<div className="form-group">
+						<input className="form-control" type="text" placeholder="Asunto" />
+					</div>
+					<div className="form-group">
+						<textarea className="form-control" rows="4" placeholder="Deja tu mensaje aqui" />
+					</div>
+					<div className="text-center m-2">
+						<button type="submit" className="btn btn-success">Enviar</button>
+					</div>
+				</form>
+			</div>
+		</div>
+  )
 }
 
 export default ContactoForm;
--- src/containers/Contacto/Contacto.js
+++ src/containers/Contacto/Contacto.js
@@ -5,9 +5,10 @@ import ContactoForm from '../../components/Contacto/ContactoForm';
 class Contacto extends Component {
   render() {
     return (
-      <section class="container mt-3">
-        <h3 class="text-center text-uppercase">Contactanos</h3>
-        <p class="lead text-center mt-3 ">Si tienes alguna pregunta puedes escribirnos y con gusto te responderemos en cuanto podamos</p>
+      <section class="container my-5">
+        <h4 className="display-4 text-center">Contáctanos</h4>
+        <p class="lead text-center mt-3 ">Si tienes alguna pregunta puedes escribirnos y con gusto te responderemos en cuanto podamos.</p>
+        <hr />
         <ContactoForm />
       </section> 
     )
